0a2c403c56da34372d029db037675f134e1b6b09
Mujaffarkhan Sanadi <mujaffarkhan.sanadi@clariontechnologies.co.in>
2017-01-04 15:35:45 +0530

    asdfasdf

---
+++ js/init.js
@@ -0,0 +1,73 @@
+var admobid = {}
+if (/(android)/i.test(navigator.userAgent)) {  // for android & amazon-fireos
+    admobid = {
+        banner: 'ca-app-pub-3868593263837372/8649306643',
+        interstitial: 'ca-app-pub-3868593263837372/5758755042',
+    }
+}
+
+function onLoad() {
+    if ((/(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent))) {
+        document.addEventListener('deviceready', initApp, false);
+    } else {
+        initApp();
+    }
+}
+
+function initApp() {
+
+    window.addEventListener('filePluginIsReady', function () {
+        alert('File plugin is ready');
+
+        window.requestFileSystem(window.TEMPORARY, 5 * 1024 * 1024, function (fs) {
+
+            console.log('file system open: ' + fs.name);
+            createFile(fs.root, "newTempFile.txt", false);
+
+        }, onErrorLoadFs);
+
+    }, false);
+    
+}
+
+function createFile(dirEntry, fileName, isAppend) {
+    alert('Inside create file');
+    // Creates a new file or returns the file if it already exists.
+    dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry) {
+
+        writeFile(fileEntry, null, isAppend);
+
+    }, onErrorCreateFile);
+
+}
+
+function writeFile(fileEntry, dataObj) {
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function (fileWriter) {
+
+        fileWriter.onwriteend = function() {
+            console.log("Successful file write...");
+            readFile(fileEntry);
+        };
+
+        fileWriter.onerror = function (e) {
+            console.log("Failed file write: " + e.toString());
+        };
+
+        // If data object is not passed in,
+        // create a new Blob instead.
+        if (!dataObj) {
+            dataObj = new Blob(['some file data'], { type: 'text/plain' });
+        }
+
+        fileWriter.write(dataObj);
+    });
+}
+
+function onErrorLoadFs(){
+    alert('onErrorLoadFs');
+}
+
+function onErrorCreateFile(){
+    alert('onErrorCreateFile');
+}
